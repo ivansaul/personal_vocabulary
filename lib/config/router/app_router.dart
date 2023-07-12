5f1e39732c5b74ef56d295bae7d2cdd3c44ddcce
BrunoCura2020 <78822885+BrunoCura2020@users.noreply.github.com>
2023-07-12 19:32:42 -0300

    Update app_router.dart

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,19 @@
-import 'package:flutter_template/presentation/screens/home_screen.dart';
+import 'package:flutter_template/ui/screens/home_screen.dart';
+import 'package:flutter_template/ui/screens/second_page.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen(),
+    // ),
     GoRoute(
       path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      name: SecondPage.name,
+      builder: (context, state) => const SecondPage(),
     )
   ],
 );
